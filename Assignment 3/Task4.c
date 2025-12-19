36fae4170f016c7d21f7a2a7719f45fad1698afc
GakereEugene <gakereeugene@gmail.com>
2025-12-19 23:11:46 +0300

    Add files via upload

---
+++ Assignment 3/Task4.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 5, num2 = 15;
+    int *ptr1, *ptr2;
+    int sum;
+
+    ptr1 = &num1;
+    ptr2 = &num2;
+
+    sum = *ptr1 + *ptr2;
+
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
---
+++ Assignment 3/Task6.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+void incrementByValue(int x)
+{
+    x = x + 1;
+    printf("Inside incrementByValue: %d\n", x);
+}
+
+void incrementByReference(int *x)
+{
+    *x = *x + 1;
+    printf("Inside incrementByReference: %d\n", *x);
+}
+
+int main()
+{
+    int num = 10;
+
+    incrementByValue(num);
+    printf("After incrementByValue: %d\n", num);
+
+    incrementByReference(&num);
+    printf("After incrementByReference: %d\n", num);
+
+    return 0;
+}
